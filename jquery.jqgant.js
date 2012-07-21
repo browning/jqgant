d3ca10424d9ec4b17aa14f7da012a48b7c684e3f
Brian Browning <brian@brian-VAIO.gdsprod.com>
2012-07-21 21:17:42 +0200

    fix end date calculations so they take into account weekends

--- jquery.jqgant.js
+++ jquery.jqgant.js
@@ -70,7 +70,21 @@
 	// utility funciton that adds a number of workdays to the date
 	// takes into account weekends.
 	var add_workdays_to_date = function(start_date, num_workdays) {
-		 return start_date.addDays(num_workdays);
+		num_days = num_workdays;
+		if ( start_date.getDay() + num_workdays < 6)
+		{
+			// don't need to add any weekends
+		}	
+		else
+		{
+			// add the weekends
+			num_days = num_days +  2 * Math.floor(num_days / 5);
+			if ( num_workdays % 5 + start_date.getDay() > 5)
+			{
+				num_days += 2;
+			}
+		}
+		return start_date.addDays(num_days);
 	}
 
 	// this function is called to update the json task object 

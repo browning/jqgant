58ba1c90807f9493f81b8b3db21a08426059deb1
Brian Browning <brian@brian-VAIO.gdsprod.com>
2012-07-23 21:58:05 +0200

    fix dragging bug on scroll

--- jquery.jqgant.js
+++ jquery.jqgant.js
@@ -129,12 +129,12 @@
 							     $("#task" + task_id).offset().top],
 					stop: function(event, ui) {
 						// snap to borders
-						$(event.target).offset({left: Math.floor($(event.target).offset().left / 22) * 22,
+						$(event.target).offset({left: Math.floor($(event.target).offset().left / 22) * 22 + 22 - $(".cellcontainer").scrollLeft() %22,
 									top: $(event.target).offset().top});	
 						update_task($(event.target).data('task_id'),
 							add_workdays_to_date(grid_start_date, 
 								Math.floor(($(event.target).offset().left -
-									 $("#cell_0_0").offset().left  )/ 22)),
+									$("#cell_0_0").offset().left  )/ 22)),
 							Math.floor($(event.target).width() / 22));	
 							}});
 	}
